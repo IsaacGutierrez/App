5d1349a33b09e2dc68b834f1147b65782c9aef87
IsaacGutierrez <Inggutierrez1113@hotmail.com>
2016-01-20 16:58:16 -0400

    change the second row with my full  name

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 alert("hello");
 function hello(){
 	document.getElementById("hola");
-	document.getElementById("hola");
+	document.getElementById("isaac gutierrez");
 	document.getElementById("isaac");
 	document.getElementById("hola");
 }
