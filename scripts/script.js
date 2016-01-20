c0fb9dfe5fc75f5d700412126ce1d7f2d370bedf
IsaacGutierrez <Inggutierrez1113@hotmail.com>
2016-01-20 16:56:40 -0400

    hello

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@ alert("hello");
 function hello(){
 	document.getElementById("hola");
 	document.getElementById("hola");
-	document.getElementById("hola");
+	document.getElementById("isaac");
 	document.getElementById("hola");
 }
